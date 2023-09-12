ceec68e9c9e677445d875e49a0f148f73558462e
Supratim Mondal <supratimmondal50@gmail.com>
2023-09-12 06:24:15 +0530

    My practice projects till the time I commit the code

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style={{backgroundColor: "olive"}}>
+        hey
+      </div>
+    </>
+  )
+}
+
+export default App

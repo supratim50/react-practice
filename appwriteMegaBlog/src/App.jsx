0ae4a9e7951c9ec98948f38034baee109d9ececa
Supratim Mondal <supratimmondal50@gmail.com>
2023-09-24 21:03:13 +0530

    Building the Auth service in appwrite

---
+++ appwriteMegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import config from "./config/config";
+
+function App() {
+  console.log(config.appwriteUrl);
+
+  return (
+    <>
+      <h1>Blog App</h1>
+    </>
+  );
+}
+
+export default App;

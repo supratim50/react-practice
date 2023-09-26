7195be90532ff5c68db13a91557bca5df98097f2
Supratim Mondal <supratimmondal50@gmail.com>
2023-09-26 21:14:42 +0530

    Configure the store using redux

--- appwriteMegaBlog/src/App.jsx
+++ appwriteMegaBlog/src/App.jsx
@@ -1,13 +1,37 @@
-import config from "./config/config";
+import {useState, useEffect} from "react";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log(config.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  );
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(userData => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() =>setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* TODO: <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (<div className="bg-green-500">Loading!!</div>)
 }
 
 export default App;

1fcb83650a1a01fe31c4fad6a34fd24dac9bd495
Supratim Mondal <supratimmondal50@gmail.com>
2023-10-04 21:03:06 +0530

    Add submit method to the postFom component

--- appwriteMegaBlog/src/components/post-form/PostForm.jsx
+++ appwriteMegaBlog/src/components/post-form/PostForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {useForm} from "react-hook-form";
 import {Button, Input, Select, RET} from "../index";
 import appWriteService from "../../appwrite/configDB";
@@ -14,6 +14,45 @@ const PostForm = ({post}) => {
             status: post?.status || 'active'
         }
     })
+
+    const navigate = useNavigate();
+    const userData = useSelector(state => state.user.userData);
+
+    const submit = async (data) => {
+      if(post) {
+        const file = data.image[0] ? appWriteService.uploadFile(data.image[0]) : null;
+
+        if(file) {
+          appWriteService.deleteFile(post.featuredImage)
+        }
+
+        const dbPost = await appWriteService.updatePost(post.$id, {
+          ...data,
+          featuredImage: file ? file.$id : undefined
+        })
+
+        if(dbPost) {
+          navigate(`/post/${dbPost.$id}`)
+        } 
+      } else {
+          const file = await appWriteService.uploadFile(data.image[0]);
+
+          if(file) {
+            const fileId = file.$id;
+            data.featuredImage = fileId;
+            const dbPost = await appWriteService.createPost({
+              ...data,
+              userId: userData.$id
+            })
+            if(dbPost) {
+              navigate(`/post/${dbPost.$id}`);
+            }
+          }
+      }
+    }
+
+    
+
   return (
     <div>PostForm</div>
   )

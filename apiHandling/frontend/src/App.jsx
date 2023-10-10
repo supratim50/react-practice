27788f2d8387cfc90d3486d92973a67686d073e1
Supratim Mondal <supratimmondal50@gmail.com>
2023-10-10 23:16:36 +0530

    learn api handling in react

--- apiHandling/frontend/src/App.jsx
+++ apiHandling/frontend/src/App.jsx
@@ -3,22 +3,40 @@ import './App.css';
 import axios from "axios"
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (
       async () => {
-      const response = await axios.get("http://localhost:3000/api/products");
-      console.log(response.data)
-      // setProducts(response);
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get("/api/products");
+        console.log(response.data)
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
       }
     )()
   }, [])
 
+  if(error) {
+    return <h1>Something Went Wrong</h1>
+  }
+
+  if(loading) {
+    return <h3>Loading...</h3>
+  }
+
   return (
     <>
-      <h1>Chai aur API in react</h1>
-      <h2>Number of Products are: {products.length}</h2>
+      <h2>Chai aur API in react</h2>
+      <h3>Number of Products are: {products.length}</h3>
     </>
   )
 }
